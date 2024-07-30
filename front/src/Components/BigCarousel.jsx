76b08861e5db38eb08eb2456f8cf5b05612a38c8
David Bensadon <bensadondavidn@gmail.com>
2024-07-30 16:29:22 +0300

    changes

--- front/src/Components/BigCarousel.jsx
+++ front/src/Components/BigCarousel.jsx
@@ -1,35 +1,36 @@
 import React, { useState } from "react";
-import Carousel from "./Carousel";
 
 const BigCarousel = () => {
-  const carousels = [
-    <Carousel key={0} />,
-    <Carousel key={1} />,
-    <Carousel key={2} />,
-    <Carousel key={3} />
+
+  const categories = [
+    { name: "SWIMWEAR", image: "swimwear.jpg" },
+    { name: "BEACHWEAR", image: "beachwear.jpg" },
+    { name: "ACCESSORIES", image: "accessories.jpg" }
   ];
   const [index, setIndex] = useState(0);
 
   const PreviousItem = () => {
-    setIndex(previousIndex => (previousIndex === 0 ? carousels.length - 1 : previousIndex - 1));
+    setIndex(previousIndex => (previousIndex === 0 ? categories.length - 1 : previousIndex - 1));
   };
 
   const NextItem = () => {
-    setIndex(previousIndex => (previousIndex === carousels.length - 1 ? 0 : previousIndex + 1));
+    setIndex(previousIndex => (previousIndex === categories.length - 1 ? 0 : previousIndex + 1));
   };
 
   return (
-    <div className="big-carousel">
-      <div>
-        <div key={index}>
-          {carousels[index]}
-        </div>
-        <div className="carousel-container">
-          <button className="previous-button" onClick={PreviousItem}>&lt;</button>
-          <button className="next-button" onClick={NextItem}>&gt;</button>
-        </div>
+    <>
+    <div className="category-carousel">
+      <div key={index} className="category">
+        <img src={categories[index].image} alt={categories[index].name} />
+        <h2>{categories[index].name}</h2>
+        <button className="shop-button">SHOP NOW</button>
       </div>
-    </div>
+      <div className="carousel-container">
+        <button className="previous-button" onClick={PreviousItem}>&lt;</button>
+        <button className="next-button" onClick={NextItem}>&gt;</button>
+      </div>
+      </div>
+    </>
   );
 };
 
--- front/src/Components/Home.jsx
+++ front/src/Components/Home.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Carousel from "./Carousel";
 import ChangeScreenWidth from "./Screenwidth";
+import BigCarousel from "./BigCarousel";
 
 const Home = () => {
 
@@ -16,11 +17,10 @@ const Home = () => {
         <img src="/plage-maillot.jpg" alt="plage" className="plage-img" />
         <div className="shop-p-btn">
           <p>DISCOVER NEW ARRIVALS</p>
-          <button className="shop-button">SHOP NOW</button>
+          <button>SHOP NOW</button>
         </div>
       </div>
       </div>
-      <p className="carousel-title">See what we do</p>
       <div className="carousel-container">
       <Carousel />
       {screenWidth > 768 ? 
@@ -31,6 +31,23 @@ const Home = () => {
       </> 
       : null}
       </div>
+      <div className="categories">
+        <div className="category">
+          <img src="/category-maillot.jpg"/>
+          <h2>SWIMWEAR</h2>
+          <button className="shop-button"> SHOP NOW</button>
+        </div>
+        <div className="category">
+          <img src="category-habits.jpg" />
+          <h2 className="cloth">CLOTHES</h2>
+          <button className="shop-button"> SHOP NOW</button>
+        </div>
+        <div className="category">
+          <img src="category-bags.jpg"/>
+          <h2 className="bags">BAGS</h2>
+          <button className="shop-button"> SHOP NOW</button>
+        </div>
+      </div>
       <hr className="hr-home" />
       <Footer />
     </>
@@ -38,3 +55,14 @@ const Home = () => {
 };
 
 export default Home;
+
+
+{/* <div className="about">
+          <h2>About us</h2>
+          <p> Welcome to Noa, where fashion meets passion. Based in the heart of Jerusalem, our brand is born from a love for style and creativity.
+              Our founder, Noa, has always had a flair for drawing and sewing, turning her dreams into reality with a beautiful line of swimwear.
+              At Noa, we believe in celebrating individuality and confidence. Each piece is designed with care, blending elegance and comfort
+              to make every woman feel special.
+              Join us on this journey of fashion and grace. Thank you for being a part of our story!
+          </p>
+      </div> */}
